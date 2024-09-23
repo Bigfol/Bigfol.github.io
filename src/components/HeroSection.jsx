0be65fca423c6d9eb3a9819be6fe982e99d052e8
KvKolokolov <kvkolokolov@kassa24.kz>
2024-09-23 22:21:41 +0500

    add 2 season

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,14 @@
 const HeroSection = () => {
   return (
     <div className="relative mx-auto mb-[85px] lg:mt-10 lg:mb-[170px] transition-all">
+      <div
+        id="nbcgate-pay-button"
+        nbcgate-order-amount="{{ --AMOUNT-- }}"
+        nbcgate-order-id="{{ --TRANSACTION ID-- }}"
+        nbcgate-return-url=""
+        nbcgate-theme="dark"
+        nbcgate-lang="ru"
+      ></div>
       <img
         src="img/hero-images-desktop.webp"
         alt="hero"
